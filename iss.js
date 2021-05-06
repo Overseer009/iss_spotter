005cd77cf3a90e6c5ff445316b1bb62783c6467b
Overseer009 <sacha.sunnasy@gmail.com>
2021-05-06 14:26:19 -0400

    completed part 1 and 2 of iss spotter

---
+++ iss.js
@@ -0,0 +1,37 @@
+const request = require("request");
+
+const fetchMyIP = function(callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    callback(null, JSON.parse(body).ip);
+
+  });
+};
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinate IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    callback(null,{ latitude: '45.501690', longitude: '-73.567253' });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
