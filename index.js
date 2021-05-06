4763488633614e963e8e8f3cf50c8db251e62f7c
Overseer009 <sacha.sunnasy@gmail.com>
2021-05-06 15:18:09 -0400

    completed iss spotter 3, now have the ISS flyover times

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -13,6 +13,15 @@ fetchCoordsByIP('70.80.224.99', (error, coordinates) => {
     console.log("It didn't work!" , error);
     return;
   }
-
   console.log('It worked! Returned coordinates:' , coordinates);
+});
+
+const coordinates = { latitude: '45.501690', longitude: '-73.567253' };
+
+fetchISSFlyOverTimes(coordinates, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
