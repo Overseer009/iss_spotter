5af49448ade09d0a9f17eac08725c319386f82fa
Overseer009 <sacha.sunnasy@gmail.com>
2021-05-06 23:26:55 -0400

    completed the iss promise edition

---
+++ index2.js
@@ -0,0 +1,20 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+const { printPassTimes } = require('./index');
+
+// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised');
+
+
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then((body) => console.log(body));
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
